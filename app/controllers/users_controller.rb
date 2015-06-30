94e5ce4097c9d12b33815b0cb7853c7680f2ea7e
tobias kappert <tobias.kappert@web.de>
2015-07-01 00:49:27 +0200

    formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,37 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :pubkey]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
   end
 
   def pubkey
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
     respond_to do |format|
       if User.exists?(:name => @user.name)
-        format.json status: 501 # { render json: '{"status":"501"}' },
+        format.json { render json: '{ "status":"501" }', status: 501 }
       elsif @user.save
-        #format.html { redirect_to @user, notice: 'User was successfully created.', status: 200 }
-        #add add statuscode
-        #render status: 200
         format.json { render json: '{ "status":"200" }', status: 200 }
       else
         format.html { render :new }
-        #render status: 500
         format.json { render json: @user.errors, status: 500 }
       end
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -58,8 +44,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
